716d7fda83a1a6e0967ad169a5f2a59d44673b2a
eliasyeme <eliasyeme@gmail.com>
2023-09-05 12:03:30 +0300

    fix pycodestyle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     upper = ""
-    for  c in str:
+    for c in str:
         c = ord(c)
         if c >= ord('a') and c <= ord('z'):
             c = c - (ord('a') - ord('A'))
