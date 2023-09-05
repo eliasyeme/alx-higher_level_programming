aa47eacb78dd0adcb9374c1ea95dfa679f58d0fd
eliasyeme <eliasyeme@gmail.com>
2023-09-05 11:57:09 +0300

    reverse alternating alphbet sounds like a spell

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def alpha_rev():
+    for c in reversed(range(ord('a'), ord('z') + 1)):
+        if c % 2 != 0:
+            c = c - (ord('a') - ord('A'))
+        print("{:c}".format(c), end="")
