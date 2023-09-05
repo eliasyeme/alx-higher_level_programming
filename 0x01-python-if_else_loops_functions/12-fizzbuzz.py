8fdf2b62a75727e6bd7192b07740c9f1e93578ed
eliasyeme <eliasyeme@gmail.com>
2023-09-05 11:00:43 +0300

    fizz buzz fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        s = ""
+        if i % 15 == 0:
+            s = "FizzBuzz"
+        elif i % 3 == 0:
+            s = "Fizz"
+        elif i % 5 == 0:
+            s = "Buzz"
+        else:
+            s = str(i)
+        print("{}".format(s), end=" ")
