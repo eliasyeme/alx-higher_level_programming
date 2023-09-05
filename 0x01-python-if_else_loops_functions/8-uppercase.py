41d2946520b1359b28f4ff030b2bc07ae7a97b9c
eliasyeme <eliasyeme@gmail.com>
2023-09-05 10:20:27 +0300

    toupper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    upper = ""
+    for  c in str:
+        c = ord(c)
+        if c >= ord('a') and c <= ord('z'):
+            c = c - (ord('a') - ord('A'))
+        upper += chr(c)
+    print(upper)
