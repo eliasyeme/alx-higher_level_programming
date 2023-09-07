ea52afac23254443e8767d10b8e4d38b2df2a93b
eliasyeme <eliasyeme@gmail.com>
2023-09-07 13:22:37 +0300

    output check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,5 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         print("{} arguments:".format(len(argv) - 1))
-        for idx, arg in enumerate(argv):
-            if idx == 0:
-                continue
-            print("{}: {}".format(idx, arg))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
