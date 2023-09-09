d85c6cdf142b76d95ce90a8e4d44f6da9d64027c
eliasyeme <eliasyeme@gmail.com>
2023-09-09 08:34:23 +0300

    fail check more return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    c = None
-    l = 0
+    chr= None
+    length = 0
     if sentence:
-        c = sentence[0]
-        l = len(sentence)
-    return (l, c)
+        chr = sentence[0]
+        length = len(sentence)
+    return (length, chr)
