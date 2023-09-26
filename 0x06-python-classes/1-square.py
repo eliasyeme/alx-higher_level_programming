179fc0c2124589b974990a4e93ff32c17d0435ce
eliasyeme <eliasyeme@gmail.com>
2023-09-26 08:09:28 +0300

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """Initialize the class
+
+        Args:
+            size (int): Size of the square
+        """
+        self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Initialize the class
+
+        Args:
+            size (int, optional): Size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
