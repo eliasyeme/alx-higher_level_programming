f8e6c51d45280edcbea2cb0c3f4e453dde0d62ff
eliasyeme <eliasyeme@gmail.com>
2023-09-27 09:34:45 +0300

    add_integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+""" Add module """
+
+
+def add_integer(a, b=98):
+    """Add two integers
+    Args:
+        a (int | float): first number
+        b (int | float): second number
+    Returns:
+        addition of the a and b
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
