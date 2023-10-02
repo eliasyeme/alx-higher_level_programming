c0d5cafe43fea99a47c324115cfdc6da85fa2a11
eliasyeme <eliasyeme@gmail.com>
2023-10-02 08:52:34 +0300

    width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+"""Module for Rectangle"""
+
+
+class Rectangle:
+    """Rectangle class
+
+    Attributes:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+    """
+    def __init__(self, width: int=0, height: int=0) -> None:
+        """Initialize rectangle class
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self) -> int:
+        """Get width"""
+        return self.__width
+
+    @width.setter
+    def width (self, value: int) -> None:
+        """Set width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self) -> int:
+        """Get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value) -> None:
+        """Set height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
