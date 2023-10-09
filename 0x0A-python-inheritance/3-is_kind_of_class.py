06647b8ad39f4022b0dfce3ed298ed4258841f11
eliasyeme <eliasyeme@gmail.com>
2023-10-09 09:23:51 +0300

    Check soft instance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Check instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check instance of an object
+    Args:
+        obj (object): base object
+        a_class (any): class to check
+    """
+    return isinstance(obj, a_class)
