a13ebb1c86fa5a5865969abdac21cf6d73962093
eliasyeme <eliasyeme@gmail.com>
2023-10-09 09:43:11 +0300

    Check instance class inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Check inheritance of a class"""
+
+
+def inherits_from(obj, a_class):
+    """Check inheritance from a class
+    Args:
+        obj (object): base object
+        a_class (any): class to check
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
