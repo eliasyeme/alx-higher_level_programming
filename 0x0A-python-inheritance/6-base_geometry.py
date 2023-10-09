c59b6cf30ab9babf72f45d75e65542740908987b
eliasyeme <eliasyeme@gmail.com>
2023-10-09 10:22:54 +0300

    Unimplemented area method

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
