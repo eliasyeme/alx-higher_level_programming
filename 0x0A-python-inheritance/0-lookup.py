b71afff8201632319240a9869ab928791b905c00
eliasyeme <eliasyeme@gmail.com>
2023-10-09 08:17:07 +0300

    Lookup attributes and method of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Lookup attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Lookup attributes and methods of an object
+
+    Args:
+        obj (object): object to lookup
+    Return:
+        list: list of objects
+    """
+    return dir(obj)
