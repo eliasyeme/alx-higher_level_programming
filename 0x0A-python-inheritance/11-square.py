0da1bb1565e2c7a6e6070dea5773c35a48635e49
eliasyeme <eliasyeme@gmail.com>
2023-10-09 11:28:00 +0300

    Nothing

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Square module"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        """Initialize square
+
+        Args:
+            size (int): size of the square.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
