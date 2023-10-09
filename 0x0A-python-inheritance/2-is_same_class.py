16b95284b4c1130f5f64c5a9cdfbe1f01e3a89dd
eliasyeme <eliasyeme@gmail.com>
2023-10-09 09:22:03 +0300

    Check Exact instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Check instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """Check instance of an object
+    Args:
+        obj (object): base object
+        a_class (any): class to check
+    """
+    return type(obj) is a_class
