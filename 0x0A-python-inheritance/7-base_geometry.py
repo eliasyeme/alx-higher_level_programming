d0a23c5c2027b3522046333db486f7fa556dc6b7
eliasyeme <eliasyeme@gmail.com>
2023-10-09 10:23:11 +0300

    Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Integer validator
+
+        Args:
+            name (str): Name of parameter.
+            value (int): Parameter to validate.
+
+        Raises:
+            TypeError: If value is not integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
