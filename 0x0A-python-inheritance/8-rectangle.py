902de82a363823b8d980cca625f8bd70d552e097
eliasyeme <eliasyeme@gmail.com>
2023-10-09 11:11:31 +0300

    Change copy class to import

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,29 +1,6 @@
 #!/usr/bin/python3
-"""Geometry module"""
-
-
-class BaseGeometry:
-    """Geometry class"""
-    def area(self):
-        """Not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Integer validator
-
-        Args:
-            name (str): Name of parameter.
-            value (int): Parameter to validate.
-
-        Raises:
-            TypeError: If value is not integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) is not int:
-            raise TypeError("<name> must be an integer")
-
-        if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+"""Rectangle module"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
