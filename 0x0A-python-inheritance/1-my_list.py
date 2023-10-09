2be0c46907710c89e651aacb23a47fa343be1f46
eliasyeme <eliasyeme@gmail.com>
2023-10-09 09:10:48 +0300

    Inhearited from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Define inherited list from list class"""
+
+
+class MyList(list):
+    """Inherits from list class"""
+    def print_sorted(self):
+        """Sort list ascending"""
+        print(sorted(self))
