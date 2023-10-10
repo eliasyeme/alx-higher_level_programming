b99562f41997c7c6f8e2ba468e29053257fa2cc3
eliasyeme <eliasyeme@gmail.com>
2023-10-10 08:42:06 +0300

    Append to a file create if it doesn't exist

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Append string to file"""
+
+
+def append_write(filename="", text=""):
+    """Append string to file as utf-8 encoding
+
+    Args:
+        filname (str): file to write to
+        text (str): text to write
+    """
+    with open(filename, "a", encoding="UTF8") as file:
+        return file.write(text)
