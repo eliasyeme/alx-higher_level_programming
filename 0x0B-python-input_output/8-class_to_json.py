f7389297667cba28ec07a1a7ac047e49ad63ab51
eliasyeme <eliasyeme@gmail.com>
2023-10-10 11:56:19 +0300

    Dict list datastructure simple

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Dictionary discription"""
+
+
+def class_to_json(obj):
+    """Dictionary description
+
+    Args:
+        obj (any): Serializable class
+
+    Return:
+        dictionary description with simple
+        data structure
+    """
+    return obj.__dict__
