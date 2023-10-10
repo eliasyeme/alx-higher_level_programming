bb134126e362223ce607c0733436810e619140f2
eliasyeme <eliasyeme@gmail.com>
2023-10-10 11:11:03 +0300

    Object from json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Load json from file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Load json to an object from a file
+
+    Args:
+        filename (str): file to write from
+
+    Returns:
+        An object read from file
+    """
+    with open(filename, "r") as file:
+        return json.load(file)
