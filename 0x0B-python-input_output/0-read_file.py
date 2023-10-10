316722b57738d04fac8f187bc3a2aadd401e1662
eliasyeme <eliasyeme@gmail.com>
2023-10-10 08:22:26 +0300

    Read from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Module to read file"""
+
+def read_file(filename=""):
+    """Read file as utf-8 encoding
+
+    Args:
+        filename (str): Name of the file to read from
+    """
+    with open(filename, "r", encoding="UTF8") as file:
+        print(file.read(), end="")
