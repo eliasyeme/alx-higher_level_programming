18114fca220b5ccdbbbb2a5a50d657d855eaf754
eliasyeme <eliasyeme@gmail.com>
2023-10-11 18:54:53 +0300

    fix

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,4 +11,4 @@ def save_to_json_file(my_obj, filename):
         filename (str): file to write to
     """
     with open(filename, "w") as file:
-        json.dump(my_obj, file, indent=2)
+        json.dump(my_obj, file)
