a78a4c65240813e842d0f38e47c3aa845d1bc47c
eliasyeme <eliasyeme@gmail.com>
2023-10-12 10:26:33 +0300

    Add base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Base class module"""
+
+
+class Base:
+    """Base class to be based on
+
+    Attributes:
+        __nb_objects (int): Number of instances from base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None) -> None:
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
