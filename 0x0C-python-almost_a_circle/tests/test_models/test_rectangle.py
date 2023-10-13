6f165045e9c56e3fd487c9ba55dfb9de1b3c3e75
eliasyeme <eliasyeme@gmail.com>
2023-10-13 07:59:51 +0300

    Organize rectangle test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,64 +5,14 @@ from models.rectangle import Rectangle
 
 
 class TestRectagle(unittest.TestCase):
-
-    def setUp(self):
-        self.r2_10x2 = Rectangle(10, 2)
-        self.r2_1x7 = Rectangle(1, 7)
-        self.r3_10x2_9 = Rectangle(10, 2, 9)
-        self.r3_2x10_6 = Rectangle(2, 10, 6)
-        self.r4_10x2_6_9 = Rectangle(10, 2, 6, 9)
-        self.r4_2x10_9_6 = Rectangle(2, 10, 9, 6)
-        self.r5_10x2_6_9_12 = Rectangle(10, 2, 6, 9, 12)
-        self.r5_2x10_9_6_21 = Rectangle(2, 10, 9, 6, 21)
-
-    def tearDown(self):
-        pass
-
+    """Base rectangle class test"""
     def test_rect_is_instance_of_base(self):
         self.assertIsInstance(Rectangle(1, 1), Base)
 
     def test_rect_id(self):
-        id1 = self.r2_10x2.id
-        id2 = self.r2_1x7.id - 1
-        self.assertEqual(id1, id2)
-
-    def test_rect_get_width(self):
-        self.assertEqual(self.r2_1x7.width, 1)
-
-    def test_rect_get_height(self):
-        self.assertEqual(self.r2_1x7.height, 7)
-
-    def test_rect_get_x_default(self):
-        self.assertEqual(self.r2_1x7.x, 0)
-
-    def test_rect_get_y_default(self):
-        self.assertEqual(self.r2_1x7.y, 0)
-
-    def test_rect_get_already_set_x(self):
-        self.assertEqual(self.r3_10x2_9.x, 9)
-
-    def test_rect_get_already_set_x_with_y(self):
-        self.assertEqual(self.r4_10x2_6_9.x, 6)
-
-    def test_rect_get_already_set_y_with_x(self):
-        self.assertEqual(self.r4_10x2_6_9.y, 9)
-
-    def test_rect_set_width(self):
-        self.r2_1x7.width = 23
-        self.assertEqual(self.r2_1x7.width, 23)
-
-    def test_rect_set_height(self):
-        self.r2_1x7.height = 32
-        self.assertEqual(self.r2_1x7.height, 32)
-
-    def test_rect_set_x(self):
-        self.r2_1x7.x = 2
-        self.assertEqual(self.r2_1x7.x, 2)
-
-    def test_rect_set_y(self):
-        self.r2_1x7.y = 2
-        self.assertEqual(self.r2_1x7.y, 2)
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(1, 7)
+        self.assertEqual(r1.id, r2.id - 1)
 
     def test_no_args(self):
         with self.assertRaises(TypeError):
@@ -71,6 +21,18 @@ class TestRectagle(unittest.TestCase):
     def test_one_args(self):
         with self.assertRaises(TypeError):
             Rectangle(2)
+
+
+class TestRectangleWidth(unittest.TestCase):
+    """Rectangle width test"""
+    def test_rect_get_width(self):
+        r1 = Rectangle(1, 7)
+        self.assertEqual(r1.width, 1)
+
+    def test_rect_set_width(self):
+        r1 = Rectangle(1, 7)
+        r1.width = 23
+        self.assertEqual(r1.width, 23)
 
     def test_rect_0_width(self):
         with self.assertRaises(ValueError) as e:
@@ -83,35 +45,6 @@ class TestRectagle(unittest.TestCase):
         with self.assertRaises(ValueError) as e:
             Rectangle(0, 10)
         width_err = "width must be > 0"
-        err_msg = e.exception.__str__()
-        self.assertEqual(width_err, err_msg)
-
-    def test_rect_0_height(self):
-        with self.assertRaises(ValueError) as e:
-            Rectangle(10, 0)
-        width_err = "height must be > 0"
-        err_msg = e.exception.__str__()
-        self.assertEqual(width_err, err_msg)
-
-    def test_rect_negative_height(self):
-        with self.assertRaises(ValueError) as e:
-            Rectangle(10, -2)
-        width_err = "height must be > 0"
-        err_msg = e.exception.__str__()
-        self.assertEqual(width_err, err_msg)
-
-    def test_rect_negative_x(self):
-        with self.assertRaises(ValueError) as e:
-            Rectangle(1, 1, -2)
-        width_err = "x must be >= 0"
-        err_msg = e.exception.__str__()
-        self.assertEqual(width_err, err_msg)
-
-
-    def test_rect_negative_y(self):
-        with self.assertRaises(ValueError) as e:
-            Rectangle(1, 1, 2, -7)
-        width_err = "y must be >= 0"
         err_msg = e.exception.__str__()
         self.assertEqual(width_err, err_msg)
 
@@ -136,6 +69,32 @@ class TestRectagle(unittest.TestCase):
         err_msg = e.exception.__str__()
         self.assertEqual(width_err, err_msg)
 
+
+class TestRectangleHeight(unittest.TestCase):
+    """Rectangle height test"""
+    def test_rect_get_height(self):
+        r1 = Rectangle(1, 7)
+        self.assertEqual(r1.height, 7)
+
+    def test_rect_set_height(self):
+        r1 = Rectangle(1, 7)
+        r1.height= 32
+        self.assertEqual(r1.height, 32)
+
+    def test_rect_0_height(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(10, 0)
+        width_err = "height must be > 0"
+        err_msg = e.exception.__str__()
+        self.assertEqual(width_err, err_msg)
+
+    def test_rect_negative_height(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(10, -2)
+        width_err = "height must be > 0"
+        err_msg = e.exception.__str__()
+        self.assertEqual(width_err, err_msg)
+
     def test_rect_height_type_float(self):
         with self.assertRaises(TypeError) as e:
             Rectangle(1, 3.77)
@@ -154,6 +113,33 @@ class TestRectagle(unittest.TestCase):
         with self.assertRaises(TypeError) as e:
             Rectangle(1, None)
         width_err = "height must be an integer"
+        err_msg = e.exception.__str__()
+        self.assertEqual(width_err, err_msg)
+
+
+class TestRectangleX(unittest.TestCase):
+    """Rectangle x test"""
+    def test_rect_get_x_default(self):
+        r1 = Rectangle(1, 7)
+        self.assertEqual(r1.x, 0)
+
+    def test_rect_get_already_set_x(self):
+        r1 = Rectangle(10, 2, 9)
+        self.assertEqual(r1.x, 9)
+
+    def test_rect_get_already_set_x_with_y(self):
+        r1 = Rectangle(10, 2, 6, 9)
+        self.assertEqual(r1.x, 6)
+
+    def test_rect_set_x(self):
+        r1 = Rectangle(1, 7)
+        r1.x = 8
+        self.assertEqual(r1.x, 8)
+
+    def test_rect_negative_x(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(1, 1, -2)
+        width_err = "x must be >= 0"
         err_msg = e.exception.__str__()
         self.assertEqual(width_err, err_msg)
 
@@ -178,6 +164,29 @@ class TestRectagle(unittest.TestCase):
         err_msg = e.exception.__str__()
         self.assertEqual(width_err, err_msg)
 
+
+class TestRectangleY(unittest.TestCase):
+    """Rectangle y test"""
+    def test_rect_get_y_default(self):
+        r1 = Rectangle(1, 7)
+        self.assertEqual(r1.y, 0)
+
+    def test_rect_get_already_set_y_with_x(self):
+        r1 = Rectangle(10, 2, 6, 9)
+        self.assertEqual(r1.y, 9)
+
+    def test_rect_set_y(self):
+        r1 = Rectangle(1, 7)
+        r1.y = 2
+        self.assertEqual(r1.y, 2)
+
+    def test_rect_negative_y(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(1, 1, 2, -7)
+        width_err = "y must be >= 0"
+        err_msg = e.exception.__str__()
+        self.assertEqual(width_err, err_msg)
+
     def test_rect_y_type_str(self):
         with self.assertRaises(TypeError) as e:
             Rectangle(1, 1, 1, "string")
@@ -199,15 +208,16 @@ class TestRectagle(unittest.TestCase):
         err_msg = e.exception.__str__()
         self.assertEqual(width_err, err_msg)
 
-    def test_rect_area(self):
-        self.assertEqual(self.r2_1x7.area(), 7)
-        self.assertEqual(self.r2_10x2.area(), 20)
-        self.assertEqual(self.r3_10x2_9.area(), 20)
-        self.assertEqual(self.r3_2x10_6.area(), 20)
-        self.assertEqual(self.r4_10x2_6_9.area(), 20)
-        self.assertEqual(self.r4_2x10_9_6.area(), 20)
-        self.assertEqual(self.r5_10x2_6_9_12.area(), 20)
-        self.assertEqual(self.r5_2x10_9_6_21.area(), 20)
+
+class TestRectangleArea(unittest.TestCase):
+    """Rectangle area test"""
+    def test_rect_area_small(self):
+        r1 = Rectangle(2, 2)
+        self.assertEqual(r1.area(), 4)
+
+    def test_rect_area_medium(self):
+        r1 = Rectangle(200, 400)
+        self.assertEqual(r1.area(), 80000)
 
     def test_rect_area_anon(self):
         self.assertEqual(Rectangle(3, 7).area(), 21)
@@ -221,14 +231,17 @@ class TestRectagle(unittest.TestCase):
             Rectangle(2, 7).area(2)
 
     def test_rect_area_modified_width(self):
-        self.r2_10x2.width = 12
-        self.assertEqual(self.r2_10x2.area(), 24)
+        r1 = Rectangle(2, 2)
+        r1.width = 33
+        self.assertEqual(r1.area(), 66)
 
     def test_rect_area_modified_height(self):
-        self.r2_10x2.height = 33
-        self.assertEqual(self.r2_10x2.area(), 330)
+        r1 = Rectangle(10, 2)
+        r1.height = 33
+        self.assertEqual(r1.area(), 330)
 
     def test_rect_area_modified_width_height(self):
-        self.r2_10x2.height = 33
-        self.r2_10x2.width = 33
-        self.assertEqual(self.r2_10x2.area(), 1089)
+        r1 = Rectangle(10, 2)
+        r1.width = 11
+        r1.height = 22
+        self.assertEqual(r1.area(), 242)
