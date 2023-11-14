ca9c0c05f1206d670f89e3b23f1af6e46e280387
eliasyeme <eliasyeme@gmail.com>
2023-11-14 11:31:14 +0300

    Added script to show score and name from second_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,3 @@
 -- Creates a table called first_table in the current database in MySQL server.
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+CREATE TABLE
+    IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
