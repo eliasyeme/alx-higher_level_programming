6c182f3b0cf263cbed3f8e1a539028724d86df0e
eliasyeme <eliasyeme@gmail.com>
2023-11-15 10:26:43 +0300

    Added script to select

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,13 @@
+-- Lists all cities contained in the database hbtn_0d_usa.
+-- Each record will display: cities.id - cities.name - states.name
+-- Results is sorted in ascending order by cities.id
+-- The database name will be passed as an argument of the mysql command
+SELECT
+    cities.id,
+    cities.name,
+    states.name
+FROM
+    cities
+    LEFT JOIN states ON states.id = cities.state_id
+ORDER BY
+    cities.id;
