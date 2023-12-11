f26aebed1414bec629a739ae5152e553ec48faa9
eliasyeme <eliasyeme@gmail.com>
2023-12-11 14:27:40 +0300

    reapeat

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const num = Number(process.argv[2]);
+
+if (num) {
+  for (let i = num; i > 0; i--) {
+    console.log('x'.repeat(num));
+  }
+} else {
+  console.log('Missing size');
+}
