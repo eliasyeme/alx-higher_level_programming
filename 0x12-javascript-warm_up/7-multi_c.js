387047fd68871575f557a635730d3698af114357
eliasyeme <eliasyeme@gmail.com>
2023-12-11 14:15:10 +0300

    no it's not fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const cis = 'C is fun';
+const num = Number(process.argv[2]);
+
+if (num) {
+  for (let i = num; i > 0; i--) {
+    console.log(cis);
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
