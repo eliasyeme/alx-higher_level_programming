a016e0791a0448896a95b29110bdb0f060247cb9
eliasyeme <eliasyeme@gmail.com>
2023-12-11 13:31:12 +0300

    getting arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+
+console.log(argv.length);
+if (argv.length <= 2) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
+}
