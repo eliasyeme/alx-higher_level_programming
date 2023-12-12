8721cbc829cd6099f3b4a30442fd369957481cac
eliasyeme <eliasyeme@gmail.com>
2023-12-12 12:02:46 +0300

    add alternative print character for square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const MainSquare = require('./5-square');
+
+class Square extends MainSquare {
+  charPrint (c) {
+    const char = c || 'X';
+    console.log(`${char.repeat(this.width)}\n`.repeat(this.height).trimEnd());
+  }
+}
+
+module.exports = Square;
