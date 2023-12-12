ef10e4381d9d239965ef22ff6b79c91eea5544ee
eliasyeme <eliasyeme@gmail.com>
2023-12-12 11:08:16 +0300

    add print method to Rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(`${'X'.repeat(this.width)}\n`.repeat(this.height).trimEnd());
+  }
+}
+
+module.exports = Rectangle;
