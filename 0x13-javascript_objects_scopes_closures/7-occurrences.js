0be75923ae7a0ccd6ab85716bc2b399aba5672ed
eliasyeme <eliasyeme@gmail.com>
2023-12-12 12:19:19 +0300

    count occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  return list.filter(e => e === searchElement).length;
+  // list.forEach(e => {if (e === searchElement) count++})
+  // return count;
+};
