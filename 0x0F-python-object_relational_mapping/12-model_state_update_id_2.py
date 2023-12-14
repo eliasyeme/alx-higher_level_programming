07f171448338889a2e40394725648fda3f93ccdf
eliasyeme <eliasyeme@gmail.com>
2023-12-14 16:10:19 +0300

    add python script to add new state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+changes the name of a State object from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, update
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    port = 3306
+    state_name = "New Mexico"
+    state_id = 2
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:{}/{}".format(
+            user_name, password, port, db_name
+        ),
+        pool_pre_ping=True,
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(State).filter(State.id == state_id).update(
+        {State.name: state_name}
+    )
+    session.commit()
+    session.close()
