9b75392d6d592f8d81c7e3a319844c8dd7906cc7
eliasyeme <eliasyeme@gmail.com>
2023-12-14 15:01:56 +0300

    add python script to print the first State object from hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+prints the first State object from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    port = 3306
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:{}/{}".format(
+            user_name, password, port, db_name
+        )
+    )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).first()
+    if instance:
+        print("{}: {}".format(instance.id, instance.name))
+    else:
+        print("Nothing")
