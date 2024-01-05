5a0e62332dc48cdf38bedf2f2978745f16c0c591
eliasyeme <eliasyeme@gmail.com>
2024-01-05 14:39:51 +0300

    Technical interview prep

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,7 @@
 """ Finds a peak in a list of unsorted integers """
 
 
-def find_peak_in_list(list_of_integers):
+def find_peak(list_of_integers):
     """ Finds a peak in a list of unsorted integers """
 
     if not list_of_integers:
