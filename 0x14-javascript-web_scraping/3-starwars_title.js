f8dbea377698782a5d9c08c730054a164c0078f7
eliasyeme <eliasyeme@gmail.com>
2024-01-30 10:56:21 +0300

    Add script to read Star Wars movie api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// Script that prints the title of a Star Wars movie
+// where the episode number matches a given integer.
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request.get(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
