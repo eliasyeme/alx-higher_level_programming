db8a4a29df21e808c8507a16a16b2cdb2c88ff7f
eliasyeme <eliasyeme@gmail.com>
2024-01-30 10:44:54 +0300

    Add script to write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// Script that writes a string to a file.
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(file, content, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+});
