8d212f4cb548ac1cfbf03f0caeb528c5153b2416
eliasyeme <eliasyeme@gmail.com>
2024-01-30 11:16:52 +0300

    Add script to compute number of tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+// Script that computes the number of tasks completed by user id.
+const request = require('request');
+const url = process.argv[2];
+const completed = {};
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(body);
+    for (const result of results) {
+      if (result.completed === true) {
+        if (completed[result.userId] === undefined) {
+          completed[result.userId] = 1;
+        } else {
+          completed[result.userId] += 1;
+        }
+      }
+    }
+    console.log(completed);
+  }
+});
