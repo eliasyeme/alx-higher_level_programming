fca5cca357b3cd98e29bc24c5f9693c85673b715
eliasyeme <eliasyeme@gmail.com>
2024-01-30 11:01:33 +0300

    Add script to count movies api

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+// Script that prints the number of movies where the character
+// “Wedge Antilles” is present.
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+request.get(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(body).results;
+    for (const result of results) {
+      for (const character of result.characters) {
+        if (character.includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
